1fdf50be619b05e0e4126e374830a261e7a080c4
Chitrang-Bhoir <69299395+Chitrang-Bhoir@users.noreply.github.com>
2022-04-06 20:11:09 +0530

    Fetching Data in usable format

--- classes/appdata.py
+++ classes/appdata.py
@@ -1,9 +1,14 @@
-from classes.database import Downloads, Terms, Urls, Visits
+from database import Downloads, Terms, Urls, Visits
+import pandas as pd
+import os
+import datetime
 
 
 class AppData(object):
     def __init__(self) -> None:
-        pass
+        if not os.path.exists('viw.csv'):
+            viw = {'viw': [0, 0, 0, 0, 0, 0, 0]}
+            pd.DataFrame(viw).to_csv('viw.csv')
 
     def download(self) -> None:
         '''to be overriden by children'''
@@ -16,7 +21,8 @@ class AppData(object):
 
 class NoOfWebsites(AppData, Urls):
     def __init__(self, username: str) -> None:
-        super().__init__(username)
+        Urls.__init__(self, username)
+        AppData.__init__(self)
         self._no_of_websites: int = None
 
     def download(self) -> None:
@@ -24,13 +30,23 @@ class NoOfWebsites(AppData, Urls):
         pass
 
     def get_data(self) -> None:
-        '''call Urls.get_website_count() an store in no_of_websites'''
-        pass
+        '''call Urls.get_website_count() and store in no_of_websites'''
+        self.update()
+        self._no_of_websites = self.get_website_count()
+        with open('lu.txt', 'r') as lu:
+            if lu.readline() != datetime.datetime.now().strftime("%Y-%m-%d"):
+                df = pd.read_csv('viw.csv')
+                viw = {'viw':df['viw'].tolist()}
+                viw['viw'] = viw['viw'][1:]
+                viw['viw'].append(self._no_of_websites)
+                pd.DataFrame(viw).to_csv('viw.csv')
+        return self._no_of_websites
 
 
-class MostVisitedWebsites(AppData. Visits):
+class MostVisitedWebsites(AppData, Visits):
     def __init__(self, username: str) -> None:
-        super().__init__(username)
+        Visits.__init__(self, username)
+        AppData.__init__(self)
         self._most_visited_websites: list[str] = None
 
     def download(self) -> None:
@@ -41,12 +57,14 @@ class MostVisitedWebsites(AppData. Visits):
         '''call Visits.get_visits() and store top 5 in most_visited_websites\n
            create a pie chart\n
            update visits in week file(where number of visits in last 7 days is stored)'''
-        pass
+        self.update()
+        self._most_visited_websites = self.get_visits()
+        return self._most_visited_websites
 
 
 class VisitsInWeek(AppData):
     def __init__(self, username: str) -> None:
-        super().__init__(username)
+        AppData.__init__(self)
         self._visits_in_week: dict() = None
 
     def download(self) -> None:
@@ -56,12 +74,14 @@ class VisitsInWeek(AppData):
     def get_data(self) -> None:
         '''access visits in week file(where number of visits in last 7 days is stored) and store its data in visits_in_week\n
            create a line plot'''
-        pass
+        df = pd.read_csv('viw.csv')
+        return df['viw'].tolist()
 
 
 class WebsiteVisits(AppData, Visits):
     def __init__(self, username: str) -> None:
-        super().__init__(username)
+        Visits.__init__(self, username)
+        AppData.__init__(self)
         self._website_visits: dict() = None
 
     def download(self) -> None:
@@ -71,12 +91,15 @@ class WebsiteVisits(AppData, Visits):
     def get_data(self) -> None:
         '''call Visits.get_visits() and store it in website_visits\n
            create a bar plot'''
-        pass
+        self.update()
+        self._website_visits = self.get_visits()
+        return self._website_visits
 
 
 class NoOfDownloads(AppData, Downloads):
     def __init__(self, username: str) -> None:
-        super().__init__(username)
+        Downloads.__init__(self, username)
+        AppData.__init__(self)
         self._no_of_downloads: int = None
 
     def download(self) -> None:
@@ -85,12 +108,15 @@ class NoOfDownloads(AppData, Downloads):
 
     def get_data(self) -> None:
         '''call Downloads.get_downloads() and store it in no_of_downloads'''
-        pass
+        self.update()
+        self._no_of_downloads = len(self.get_downloads())
+        return self._no_of_downloads
 
 
 class TermsOfDay(AppData, Terms):
     def __init__(self, username: str) -> None:
-        super().__init__(username)
+        Terms.__init__(self, username)
+        AppData.__init__(self)
         self._terms_of_day: list[str] = None
 
     def download(self) -> None:
@@ -99,4 +125,26 @@ class TermsOfDay(AppData, Terms):
 
     def get_data(self) -> None:
         '''call Terms.get_terms(), perform some stemming and store it in terms_of_day(max 10)'''
-        pass
+        self.update()
+        self._terms_of_day = self.get_terms()
+        return self._terms_of_day[:10]
+
+
+if __name__ == '__main__':
+    now = NoOfWebsites('Chitrang')
+    print(now.get_data())
+
+    mvw = MostVisitedWebsites('Chitrang')
+    print(mvw.get_data())
+
+    viw = VisitsInWeek('Chitrang')
+    print(viw.get_data())
+
+    wv = WebsiteVisits('Chitrang')
+    print(wv.get_data())
+
+    nod = NoOfDownloads('Chitrang')
+    print(wv.get_data())
+
+    tod = TermsOfDay('Chitrang')
+    print(tod.get_data())
--- classes/database.py
+++ classes/database.py
@@ -1,17 +1,26 @@
 import sqlite3
 import time
 from os.path import exists
+import datetime
 
 
 class Database(object):
     def __init__(self, username: str) -> None:
         self._username = username
-        self._path = f'C:\Users\{self._username}\AppData\Local\Google\Chrome\User Data\Default\History'
+        self._path = f'C:\\Users\\{self._username}\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History'
         self._last_updated: int = None
+        self.date = datetime.datetime.now().strftime("%Y-%m-%d")
+        self.set_last_updated()
 
         '''setting up sqlite connection to be done here'''
         c = sqlite3.connect(self._path, timeout=10)
-        self._cursor = c.cursor()
+        self.cursor = c.cursor()
+
+    def get_date(self):
+        return self.date
+
+    def get_cursor(self):
+        return self.cursor
 
     def update(self) -> None:
         '''To be overriden by children'''
@@ -38,11 +47,14 @@ class Database(object):
         '''Assign current time in seconds since the epoch to last_updated\n
            Hint : Use gmtime() from datetime module'''
         self._last_updated = time.gmtime()
+        with open('lu.txt', 'w') as lu:
+            lu.write(self.date)
+        
 
         
 class Urls(Database):
     def __init__(self, username: str) -> None:
-        super.__init__(username)
+        self.db = Database(username)
         self._urls = [str]
 
     def get_urls(self) -> list[str]:
@@ -50,10 +62,15 @@ class Urls(Database):
 
     def update(self) -> None:
         '''run an sqlite query to store urls visited in urls list'''
+        self._urls = [str]
         self._urls.clear()
-        query = "SELECT DISTINCT url FROM urls;"
-        self._cursor.execute(query)
-        for i in self._cursor.fetchall():
+        query = f"SELECT urls.url \
+                    FROM visits, urls \
+                    WHERE visits.url = urls.id \
+                    AND datetime(visits.visit_time / 1000000 + (strftime('%s', '1601-01-01')), 'unixepoch') \
+                    LIKE '{self.db.get_date()}%'"
+        self.db.get_cursor().execute(query)
+        for i in self.db.get_cursor().fetchall():
             if i[0].startswith('http'):
                 self._urls.append(i[0])
 
@@ -66,33 +83,46 @@ class Urls(Database):
                 occurance += 1
             if occurance == 3:
                 break
-            return url[:pos + 1]    
+        return url[:pos + 1]    
 
     def get_website_count(self) -> int:
         '''get total number of unique domains visited in a day'''
-        pass
+        return len(self.get_total_websites())
 
     def get_total_websites(self) -> list[str]:
         '''get a list of unique domains visited in a day'''
-        pass
+        for i, url in enumerate(self._urls):
+            self._urls[i] = self.get_domain(url)
+        return list(set(self._urls))
 
 
 class Visits(Database):
     def __init__(self, username: str) -> None:
-        super().__init__(username)
-        self._visits: dict() = None
+        self.db = Database(username)
+        self._visits = {}
 
     def get_visits(self) -> dict():
         return self._visits
 
     def update(self) -> None:
         '''run an sqlite query to get number of visits on each domain in a day in a form of a dictionary'''
-        pass
+        self._visits = {}
+        query = f"SELECT url, visit_count FROM urls where \
+            datetime(last_visit_time / 1000000 + (strftime('%s', '1601-01-01')), 'unixepoch') \
+                LIKE '{self.db.get_date()}%';"
+        self.db.get_cursor().execute(query)
+        count = 0
+        for i in self.db.get_cursor().fetchall():
+            if i[0].startswith('http'):
+                self._visits[i[0]] = i[1]
+            count += 1
+            if count == 10:
+                break
 
 
 class Downloads(Database):
     def __init__(self, username: str) -> None:
-        super().__init__(username)
+        self.db = Database(username)
         self._downloads: list[str] = None
 
     def get_downloads(self) -> list[str]:
@@ -100,17 +130,30 @@ class Downloads(Database):
 
     def update(self) -> None:
         '''run an sqlite query to get a list of downloaded files in a day'''
-        pass
+        query = f"SELECT current_path \
+                    FROM downloads \
+                    WHERE datetime(end_time / 1000000 + (strftime('%s', '1601-01-01')), 'unixepoch') \
+                    LIKE '{self.get_date()}%'"
+        self.db.get_cursor().execute(query)
+        for i in self.db.get_cursor().fetchall():
+            self._downloads.append(i[0])
 
 
 class Terms(Database):
     def __init__(self, username: str) -> None:
-        super().__init__(username)
-        self._terms: list[str] = None
+        self.db = Database(username)
+        self._terms = list[str]
 
     def get_terms(self) -> list[str]:
-        return self._terms
+        return list(set(self._terms))
 
     def update(self) -> None:
         '''run an sqlite query to get a list of terms searched in a day'''
-        pass
+        query = f"SELECT keyword_search_terms.term FROM keyword_search_terms, urls WHERE\
+                        keyword_search_terms.url_id = urls.id \
+                        AND datetime(urls.last_visit_time / 1000000 + (strftime('%s', '1601-01-01')), 'unixepoch') \
+                        LIKE '{self.db.get_date()}%'"
+        self.db.get_cursor().execute(query)
+        self._terms = []
+        for i in self.db.get_cursor().fetchall():
+            self._terms.append(i[0])
